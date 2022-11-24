ef3bbc6550f79676059d8aecde5e54def1f1f890
cristino4 <117871733+cristino4@users.noreply.github.com>
2022-11-23 18:29:53 -0800

    this adds a for loop in the JS

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,23 @@
+/*
+var topic = "CSS";
+var shapes = ["triangle", "square","pentagon","circle"]
+if (topic==='HTML') {
+    console.log("Let's Study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS");
+} else if (topic === 'Git') {
+    console.log("lets study GIT!");
+} else if (topic === 'JavaScript') {
+    console.log("lets study javascript!");
+} else {
+    console.log('Please try again!');
+}
+
+for (var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x])
+}
+*/
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x])
+}  
