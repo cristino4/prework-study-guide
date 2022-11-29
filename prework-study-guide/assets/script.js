988c68269936500a6bc2ebeecb9fcc62101fc54b
cristino4 <117871733+cristino4@users.noreply.github.com>
2022-11-28 16:45:02 -0800

    A random topic is selected

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,23 +1,30 @@
-/*
-var topic = "CSS";
-var shapes = ["triangle", "square","pentagon","circle"]
-if (topic==='HTML') {
-    console.log("Let's Study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS");
-} else if (topic === 'Git') {
-    console.log("lets study GIT!");
-} else if (topic === 'JavaScript') {
-    console.log("lets study javascript!");
-} else {
-    console.log('Please try again!');
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+
+
+
+function selectTopic(){
+    if (randomTopic==='HTML') {
+        console.log("Let's Study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS");
+    } else if (randomTopic === 'Git') {
+        console.log("lets study GIT!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("lets study javascript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
 
-for (var x = 0; x < shapes.length; x++) {
-    console.log(shapes[x])
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log("topic #",x+1,":",topics[x]);
+    }
 }
-*/
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x])
-}  
+
+console.log("Here are the topics we will be working with:");
+listTopics();
+console.log("Here is a random topic we select to work on:");
+selectTopic();
